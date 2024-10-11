a691149ce6c004a51baace2d3e9d3b75e2515e5a
Haiying LUO <haiying.xie.luo@gmail.com>
2024-10-11 16:35:27 +0200

    first commit

---
+++ Test-1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Test-1
+//
+//  Created by Apprenant 166 on 11/10/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Test-1/Test_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Test_1App.swift
+//  Test-1
+//
+//  Created by Apprenant 166 on 11/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct Test_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
